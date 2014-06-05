704c4d7f4d3b5add2b959b9c7ec2c194e4aea8b5
Darren DeRidder <drderidder@gmail.com>
2014-06-05 00:31:52 -0400

    Update phi.js, programatically set margin-gap for experimental VDG multi-column layout

--- javascripts/phi.js
+++ javascripts/phi.js
@@ -1,5 +1,10 @@
 /*global window, document*/
 
+/**
+ *  Adjust CSS properties that can't be correctly set through CSS.
+ *
+ */
+
 function canonify() {
 
   // Set golden rectangle (horizontal) height
@@ -20,19 +25,19 @@ function canonify() {
   var vdg = document.getElementsByClassName("vandegraaf");
   i = vdg.length;
   var windowHeight = window.innerHeight;
-  var blockHeight, topPadding, bottomPadding;
+  var blockHeight, blockWidth, topPadding, bottomPadding;
 
   while (i--) {
+
     blockHeight = vdg[i].clientHeight;
+    blockWidth  = vdg[i].clientWidth;
     topPadding  = 0.11 * ((blockHeight < windowHeight) ? blockHeight : windowHeight);
     bottomPadding = 2 * topPadding;
     vdg[i].style.paddingTop = topPadding;
-    //if (
-    //  vdg[i].nextElementSibling && (
-    //    vdg[i].nextElementSibling.classList.contains("vandegraaf") ||
-    //    vdg[i].nextElementSibling.classList.contains("row")
-    //  )
-    //) {
+    if (vdg[i].classList.contains('multicolumn')) {
+      vdg[i].style.MozColumnGap = vdg[i].style.webkitColumnGap = vdg[i].style.columnGap = 0.11 * blockWidth + "px";
+    }
+
     if (vdg[i].classList.contains("nofooter")) {
       // collapse bottom padding for stacked VDGs
       vdg[i].style.paddingBottom = 0;
@@ -43,6 +48,11 @@ function canonify() {
 
 }
 
+/**
+ * Add event listener for window resize an adjust CSS
+ * properties accordingly
+ *
+ */
 if (window.addEventListener) {
   window.addEventListener('resize', canonify, false);
   window.addEventListener('load', canonify, false);
